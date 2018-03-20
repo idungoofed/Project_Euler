6db4644234c0b821169d19d5cec3281c504600b5
Mark Philip <philip.mark123@gmail.com>
2018-03-20 15:48:28 -0400

    Solved problem 15

---
+++ 15/p15.py
@@ -0,0 +1,9 @@
+import operator as op
+def ncr(n, r):
+    r = min(r, n-r)
+    numer = reduce(op.mul, xrange(n, n-r, -1), 1)
+    denom = reduce(op.mul, xrange(1, r+1), 1)
+    return numer//denom
+
+if __name__ == "__main__":
+    print(ncr(40, 20))
